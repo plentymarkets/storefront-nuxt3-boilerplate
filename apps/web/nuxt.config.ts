7f3fda39cd6e18961e553be983af491eb4b6d501
Noah Feist <noah.feist@plentysystems.com>
2025-01-20 15:29:59 +0100

    feat: vitalizer added without yarn.lock

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -64,6 +64,7 @@ export default defineNuxtConfig({
     },
   },
   modules: [
+    'nuxt-vitalizer',
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/test-utils/module',
@@ -78,6 +79,10 @@ export default defineNuxtConfig({
     '@vite-pwa/nuxt',
     '@vue-storefront/nuxt',
   ],
+  vitalizer: {
+    disableStylesheets: 'entry',
+    disablePrefetchLinks: true,
+  },
   vsf: {
     middleware: {
       apiUrl: validateApiUrl(process.env.API_URL) ?? 'http://localhost:8181',
